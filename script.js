ecb8e99107bd2e2e3b8db3004387d0978ce039ca
brainiac1010 <armanmahim00@gmail.com>
2024-04-28 21:25:07 +0600

    name ,number email error show

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-var nameError = document.getElementById("name-error")
-var phoneError = document.getElementById("phone-error")
-var emailError = document.getElementById("email-error")
-var messageError = document.getElementById("message-error")
-var submitError = document.getElementById("submit-error")
+var nameError = document.getElementById("name-error");
+var phoneError = document.getElementById("phone-error");
+var emailError = document.getElementById("email-error");
+var messageError = document.getElementById("message-error");
+var submitError = document.getElementById("submit-error");
 
 function validateName() {
     var name = document.getElementById('contact-name').value;
-    if (name.lenght == 0) {
+    if (name.length == 0) {
         nameError.innerHTML = 'Need a name';
         return false;
     }
@@ -16,4 +16,45 @@ function validateName() {
     }
     nameError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
     return true;
+}
+
+function validatePhone() {
+    var phone = document.getElementById('contact-phone').value;
+    if (phone.length == 0) {
+        phoneError.innerHTML = 'Need a phone No';
+        return false;
+    }
+    if (!phone.match(/^\d+$/)) {
+        phoneError.innerHTML = 'Only numbers are allowed';
+        return false;
+    }
+    if (phone.length < 11) {
+        phoneError.innerHTML = 'Phone No should be at least 11 digits';
+        return false;
+    }
+    phoneError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+    return true;
+}
+
+
+function validateEmail() {
+    var email = document.getElementById('contact-email').value;
+
+    if (email.length == 0) {
+        emailError.innerHTML = 'Email is required';
+        return false
+    }
+    if (!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
+        emailError.innerHTML = 'Email Invalid'
+        return false;
+    }
+    emailError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+
+}
+
+
+
+
+function validateMessage(){
+    
 }
